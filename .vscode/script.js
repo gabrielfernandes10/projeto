01d0cf655f87d8297646072e18a8ba9a556d3592
Gabriel Fernandes <gdasilvafernandes@outlook.com>
2025-09-04 17:31:11 -0300

    correct bugs

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,9 +1,12 @@
 function toggleMode() {
-  const html = document.documentElement
+  const html = document.documentElement;
+  const button = document.querySelector("#switch button");
+  button.classList.add("animate");
+  setTimeout(() => button.classList.remove("animate"), 300);
 
   if (html.classList.contains("light")) {
-    html.classList.remove("light")
+    html.classList.remove("light");
   } else {
-    html.classList.add("light")
+    html.classList.add("light");
   }
 }
