3d58cf256fd09b02dc89e49e4b5dbca7b3cd2638
Gabriel Fernandes <gdasilvafernandes@outlook.com>
2025-09-04 18:23:28 -0300

    correct bugs

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function toggleMode() {
   const html = document.documentElement;
   const button = document.querySelector("#switch button");
+  const icon = document.getElementById("switch-icon");
   button.classList.add("animate");
-  setTimeout(() => button.classList.remove("animate"), 300);
+  setTimeout(() => button.classList.remove("animate"), 500);
 
   if (html.classList.contains("light")) {
     html.classList.remove("light");
+    icon.src = "assets/assets/moon-stars.svg";
   } else {
     html.classList.add("light");
+    icon.src = "assets/assets/sun.svg";
   }
 }
